6d2472931c7a8d75cbc004250032cb87d7f4c361
Daniel Rosato <rosato.daniel@gmail.com>
2016-12-27 21:23:02 +0000

    initial structure

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+// const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
